5363e926b785b9b89c36ea16636915f1c2a8d0eb
Noussaiba Boughediri <noussaibaboughediri@gmail.com>
2026-05-03 19:24:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,14 +10,17 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 
+const PORT = process.env.PORT || 3000;
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('✅ اتصلنا بقاعدة البيانات بنجاح');
-    app.listen(process.env.PORT, () => {
-      console.log(`🚀 الخادم يعمل على http://localhost:${process.env.PORT}`);
-    });
   })
   .catch(err => {
     console.error('❌ خطأ في الاتصال بقاعدة البيانات:', err.message);
   });
+
+app.listen(PORT, () => {
+  console.log(`🚀 الخادم يعمل على http://localhost:${PORT}`);
+});
   
